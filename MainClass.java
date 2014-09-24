6de1d521f95b543a8bbc43086b69f9915eda0a6e
Tabetha Boushey <tabetharaeboushey@gmail.com>
2014-09-24 17:44:29 -0600

    Create MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+package menu;
+
+/**
+ *
+ * @author Kelsey O'Keefe and Tabetha Boushey
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        MenuFrame.getInstance();
+    }
+}
